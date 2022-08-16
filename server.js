45fd8122aefaad949e8e05d2f8e3261a16cbd8a0
Lexi Grant <lexidgrant@gmail.com>
2022-08-15 21:47:01 -0500

    added to the edit itenerary page, made and edit trip and delete and also added add activity

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 //Dependencies
 //___________________
 const express = require('express');
-const methodOverride  = require('method-override');
-const mongoose = require ('mongoose');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
 const Itenerary = require("./models/itenerarySchema");
 const Trip = require("./models/tripSchema");
 const Activity = require("./models/activitySchema");
 const itenerarySeed = require("./models/itenerary");
 const { response } = require('express');
-const app = express ();
+const app = express();
 const db = mongoose.connection;
 require('dotenv').config()
 //___________________
@@ -58,28 +58,76 @@ app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 //         response.redirect("/itenerary")
 //     })
 // })
-
-app.delete("/itenerary/:id", (request, response)=> {
-    Itenerary.findByIdAndRemove(request.params.id, (error, data)=> {
+// delete for itenerary
+app.delete("/itenerary/:id", (request, response) => {
+    Itenerary.findByIdAndRemove(request.params.id, (error, data) => {
         response.redirect("/itenerary")
     })
 })
 
-// Edit for trip
-app.put("/itenerary/:id/trip/:tripId/", (request, response)=> {
-    Itenerary.findById(request.params.id, (error, foundItenerary)=> {
-        const foundTrip = foundItenerary.trips.id(request.params.tripId);
-        foundTrip.set(request.body);
+// delete for trip
+app.delete("/itenerary/:id/trip/:tripId", (request, response) => {
+    Itenerary.findById(request.params.id, (error, foundItenerary) => {
+        foundItenerary.trips.id(request.params.tripId).remove()
         foundItenerary.save();
         response.redirect(`/itenerary/${request.params.id}`)
-
     })
-  
+})
+
+// create new trip
+app.get("/itenerary/:id/trip/new", (request, response) => {
+    response.render(
+        "addTrip.ejs",
+        {
+            IteneraryId: request.params.id
+        }
+    )
+})
+
+app.post("/itenerary/:id/trip/new", (request, response) => {
+    Itenerary.findById(request.params.id, (error, foundItenerary) => {
+        foundItenerary.trips.push(request.body)
+        foundItenerary.save()
+        response.redirect(`/itenerary/${request.params.id}`)
+    })
+})
+
+// create new activity
+app.get("/itenerary/:id/trip/:tripId/activity/new", (request, response) => {
+    response.render(
+        "addActivity.ejs",
+        {
+            IteneraryId: request.params.id,
+            TripId: request.params.tripId
+        }
+    )
+})
+
+app.post("/itenerary/:id/trip/:tripId/activity/new", (request, response) => {
+    Itenerary.findById(request.params.id, (error, foundItenerary) => {
+        const foundTrip = foundItenerary.trips.id(request.params.tripId)
+        foundTrip.activities.push(request.body)
+        foundItenerary.save()
+        response.redirect(`/itenerary/${request.params.id}`)
+    })
 })
 
 
-app.get("/itenerary/:id/trip/:tripId/edit", (request, response)=> {
-    Itenerary.findById(request.params.id, (error, foundItenerary)=> {
+// Edit for trip
+app.put("/itenerary/:id/trip/:tripId/", (request, response) => {
+    Itenerary.findById(request.params.id, (error, foundItenerary) => {
+        const foundTrip = foundItenerary.trips.id(request.params.tripId)
+        foundTrip.set(request.body)
+        foundItenerary.save()
+        response.redirect(`/itenerary/${request.params.id}`)
+
+    })
+
+})
+
+
+app.get("/itenerary/:id/trip/:tripId/edit", (request, response) => {
+    Itenerary.findById(request.params.id, (error, foundItenerary) => {
         const foundTrip = foundItenerary.trips.id(request.params.tripId)
         response.render(
             "editTrip.ejs",
@@ -91,73 +139,81 @@ app.get("/itenerary/:id/trip/:tripId/edit", (request, response)=> {
     })
 })
 
-// edit for itenerary
-app.put("/itenerary/:id", (request, response)=> {
-    Itenerary.findByIdAndUpdate(request.params.id, request.body, {new: true}, (error, updateItenerary)=> {
-        response.redirect("/itenerary")
+// Edit Activity
+app.put("/itenerary/:id/trip/:tripId/activity/:activityId/edit", (request, response) => {
+    const params = request.params
+    Itenerary.findById(params.id, (error, foundItenerary) => {
+        const foundTrip = foundItenerary.trips.id(params.tripId)
+        const foundActivity = foundTrip.activities.id(params.activityId)
+        foundActivity.set(request.body)
+        foundItenerary.save()
+        response.redirect(`/itenerary/${params.id}`)
     })
 })
 
-app.get("/itenerary/:id/edit", (request, response)=> {
-    Itenerary.findById(request.params.id, (error, foundItenerary)=> {
-        response.render(
-            "editItenerary.ejs",
-            {
-                Itenerary: foundItenerary
-            }
-        )
+app.get("/itenerary/:id/trip/:tripId/activity/:activityId/edit", (request, response)=> {
+
+})
+
+    // edit for itenerary
+    app.put("/itenerary/:id", (request, response) => {
+        Itenerary.findByIdAndUpdate(request.params.id, request.body, { new: true }, (error, updateItenerary) => {
+            response.redirect("/itenerary")
+        })
     })
-})
-// create new itenerary
-app.get("/itenerary/new", (request, response)=> {
-    response.render("new.ejs")
-})
 
-app.post("/itenerary/new", (request, response)=> {
-    Itenerary.create(request.body, (error, createItenerary)=> {
-        response.redirect("/itenerary")
+    app.get("/itenerary/:id/edit", (request, response) => {
+        Itenerary.findById(request.params.id, (error, foundItenerary) => {
+            response.render(
+                "editItenerary.ejs",
+                {
+                    Itenerary: foundItenerary
+                }
+            )
+        })
     })
-})
-
-// create new activity
-app.get("/itenerary/:id/trip/:id/activity", (request, response)=> {
-    response.render("addActivity.ejs")
-})
-
-app.post("itenerary/:id/trip/:tripId/activity", (request, response)=> {
-    
-})
-
-app.get("/itenerary/:id", (request, response)=> {
-    Itenerary.findById(request.params.id, (error, foundItenerary)=> {
-        response.render(
-            "show.ejs",
-            {
-                Itenerary: foundItenerary
-            }
-        )
+    // create new itenerary
+    app.get("/itenerary/new", (request, response) => {
+        response.render("new.ejs")
     })
-})
 
-app.get("/itenerary", (request, response)=> {
-    Itenerary.find({}, (error, allContents)=> {
-        console.log(allContents)
-        response.render(
-            "index.ejs",
-            {
-                Itenerary: allContents
-            }
-        )
+    app.post("/itenerary/new", (request, response) => {
+        Itenerary.create(request.body, (error, createItenerary) => {
+            response.redirect("/itenerary")
+        })
     })
-})
 
-//___________________
-//localhost:3000
-app.get('/' , (request, response) => {
-  response.redirect("/itenerary");
-});
+    // Show Page show.ejs
+    app.get("/itenerary/:id", (request, response) => {
+        Itenerary.findById(request.params.id, (error, foundItenerary) => {
+            response.render(
+                "show.ejs",
+                {
+                    Itenerary: foundItenerary
+                }
+            )
+        })
+    })
+    //  main page index.ejs
+    app.get("/itenerary", (request, response) => {
+        Itenerary.find({}, (error, allContents) => {
+            console.log(allContents)
+            response.render(
+                "index.ejs",
+                {
+                    Itenerary: allContents
+                }
+            )
+        })
+    })
 
-//___________________
-//Listener
-//___________________
-app.listen(PORT, () => console.log( 'Listening on port:', PORT));
+    //___________________
+    //localhost:3000
+    app.get('/', (request, response) => {
+        response.redirect("/itenerary");
+    });
+
+    //___________________
+    //Listener
+    //___________________
+    app.listen(PORT, () => console.log('Listening on port:', PORT));
